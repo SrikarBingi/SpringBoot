8133d9428cfa3f977263f3a7444c9522a88782be
Srikar Bingi <60936704+SrikarBingi@users.noreply.github.com>
2025-07-28 23:15:48 +0530

    Updated user, order, and springhibernate modules

--- orderservice/src/main/java/com/example/orderservice/model/Order.java
+++ orderservice/src/main/java/com/example/orderservice/model/Order.java
@@ -1,6 +1,8 @@
 package com.example.orderservice.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,6 +11,7 @@ import jakarta.persistence.Table;
 public class Order {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer oid;
     String oname;
     String status;
